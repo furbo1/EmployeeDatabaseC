e3f7d5aef330d76dc801ca1b44dad090abbbcb3b
furbo1 <al_razvan@yahoo.com>
2021-01-19 16:06:07 +0100

    adding files to github

---
+++ src/EmployeeDatabase/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmployeeDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+             string []  employees=  { "John", "Frank", "Jim", "JJ" };
+             var salaries = new []{5000, 6000, 7000, 8000};
+
+            if(employees.Length > 0) 
+            {
+                foreach(string employee in employees)
+                {
+                     Console.WriteLine(employee);
+                   
+                }
+                foreach(int salary in salaries)
+                {
+                     Console.WriteLine(salary);
+                   
+                }
+                
+            } else {
+                 Console.WriteLine("There are no employees in database");
+            }
+            
+           
+        }
+    }
+}
